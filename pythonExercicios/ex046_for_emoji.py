b695a0f0248a0c362fa41c3daa53deef3525d05f
diegops17 <psdiego17@gmail.com>
2023-08-28 22:23:44 -0300

    resolução do exercicio 046 sobre estrutura de repetição for

---
+++ pythonExercicios/ex046_for_emoji.py
@@ -0,0 +1,8 @@
+"""046 - Faça um script que mostre uma contagem regressiva para o estouro de fogos de artifício,
+indo de 10 até 0, com uma pausa de 1 segundo entre elas"""
+from time import sleep
+from emoji import emojize
+for cont in range(10, 0-1, -1):
+    print(cont)
+    sleep(1)
+print(emojize('FELIZ ANO NOVO!!! :sparkler:'))
