2675018febe7bfdc11c980a6fcf52079093a84da
diegops17 <psdiego17@gmail.com>
2023-09-13 23:29:49 -0300

    resolução do exercicio 069 while com break

---
+++ pythonExercicios/ex069.py
@@ -0,0 +1,26 @@
+"""Faça um scripr que leia a idade e o sexo de várias pessoas. A cada pessoa cadastrada
+deverá perguntar se o usuário quer continuar. No final mostrar: Quantas pessoas tem mais de 18 anos
+Quantos homens foram cadastrados. Quantas mulheres tem menos de 20 anos"""
+totalPessoasMaior18 = contHomens = contMulheresMenor20 = 0
+while True:
+    idade = int(input('idade: '))
+    sexo = str(input('Sexo: [M/F] '))[0].upper().strip()
+    while sexo not in 'MF':
+        sexo = str(input('Sexo: [M/F] '))[0].upper().strip()
+
+    if idade >= 18:
+        totalPessoasMaior18 += 1
+    if sexo == 'M':
+        contHomens += 1
+    if sexo == 'F' and idade < 20:
+        contMulheresMenor20 += 1
+
+    print('-'*30)
+    opcao = str(input('Quer continuar? [S/N] '))[0].upper().strip()
+
+    if opcao == 'N':
+        break
+print('===== FIM DO PROGRAMA =====')
+print(f'Total de pessoas com mais de 18 anos: {totalPessoasMaior18}')
+print(f'Ao todo temos {contHomens} homens cadastrados')
+print(f'São {contMulheresMenor20} com menos de 20 anos')
