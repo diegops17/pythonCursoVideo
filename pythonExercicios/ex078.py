9bf70fc373b871038dc59000f0697b87fbd1a685
diegops17 <psdiego17@gmail.com>
2023-10-01 19:49:48 -0300

    ajuste no exercicio 078

--- pythonExercicios/ex078.py
+++ pythonExercicios/ex078.py
@@ -1,20 +1,25 @@
 """Faça um script que leia 5 valores númericos e guarde-os em uma lista.
 No final mostre qual foi o maior o menor valor digitado e as suas respectivas posições na lista"""
 numeros = []
-maior = menor = posicaoMaior = posicaoMenor = 0
+maior = menor = 0
 for i in range(0, 5):
-  numeros.append(int(input('Informe um valor: ')))
-
+  numeros.append(int(input(f'Informe um valor para posição {i}: ')))
   if i == 0:
     maior = numeros[i]
     menor = numeros[i]
   else:
     if numeros[i] > maior:
         maior = numeros[i]
-        posicaoMaior = i
     elif numeros[i] < menor:
         menor = numeros[i]
-        posicaoMenor = i
 print(f'Números na lista: {numeros}')
-print(f'O maior número é {maior} e está na posição {posicaoMaior}')
-print(f'O menor número é {menor} e está na posição {posicaoMenor}')
+print(f'O maior número é {maior} e está nas posições ', end='')
+for i, v in enumerate(numeros):
+    if v == maior:
+        print(f'{i}...', end='')
+print()
+print(f'O menor número é {menor} e está nas posições ', end='')
+for i, v in enumerate(numeros):
+    if v == menor:
+        print(f'{i}...', end='')
+print()
